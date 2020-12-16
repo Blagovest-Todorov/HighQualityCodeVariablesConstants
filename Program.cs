e251a988342d0b13b3e4189a1d79818c2d6133ee
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-16 11:17:30 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VariablesExpressionsConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable must ne initialized before its usage !
+            // declare and define a variable close to where it is used!
+            // We can initialize a varialbe with its declaration.
+            // Dont define unused vartiables !
+            // use enumerations instead -> 3 defined statuses - read, write,,or read and write !
+            // always assign the resutl of some method to a variable, before retunnung it, it can help  You find the Bug when debugging !
+            // it improves the readability when debbuging !
+            // scpoe of the variable - >  the visibility of the variable for usage.
+            // global and local variables --scope -> a place fro where we can access this variable, how visible is it, 
+            // with keywords - > public, protected, nternal , private --se can set the visibility of the variable ! the level of access,
+            // global variable--then the scope is big, , but always try to reduce the scope and visibility 
+            // access all fields through its properties
+            // Variable Span should be kept  low--this is the lines of code the Varialble is defined, declared and then used /
+            // lines of code between variable usages.
+            // always declare and innittialize variable just before their usage.
+            // always define a variable where there is their first usage
+            // initialise the variable as late a spossible
+            // varaible liveeTime -- the number of lines between furst and last usage of the variable !
+            // initialize the variable used in loop befere the loop  /
+            // begin with the most restricted visibility 
+            // better grouping of the variables is important for code readability ! and undesteandability!
+            // each variable must have one purpose ! one variable ->  one purpose! 
+            // good name of the variable-describig what it does, what it is used for !
+            // the variable name should describe the object that it represents
+            // Constants in C# use pascal case naming, it is not a mistake to use all the naem i capital letters with inderscores  : FIRST_NAME ;
+            // naming conventions for quality program code !
+            // avoid _undescore and numbers in the varialble! , avoid using keywords as variable !
+            // always use english words for variables ! variable name strarts with low letter, not with capital letter!
+
+        }
+    }
+}
